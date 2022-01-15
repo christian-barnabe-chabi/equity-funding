92b065fdc703c212d75410ea2d896952c672964c
Christian Barnabé <62568487+christian-barnabe-chabi@users.noreply.github.com>
2022-01-15 07:00:24 +0000

    debuging

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(async (req, res, next) => {
 });
 */
 
-app.use(cors({origin: "*", exposedHeaders: "*"}));
+app.use(cors({origin: "https://clever-davinci-940e10.netlify.app", exposedHeaders: "*"}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
