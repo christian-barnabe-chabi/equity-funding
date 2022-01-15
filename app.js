541e667deab7c5c638ed6643285164f5152c2050
Christian Barnabé <62568487+christian-barnabe-chabi@users.noreply.github.com>
2022-01-15 06:57:37 +0000

    debuging

--- app.js
+++ app.js
@@ -40,9 +40,7 @@ app.use(async (req, res, next) => {
 });
 */
 
-// (new User).install();
-
-app.use(cors());
+app.use(cors({origin: "*", exposedHeaders: "*"}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
